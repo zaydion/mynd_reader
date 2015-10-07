e753e1ded24d5a1d30acd77409b7b35680d19074
cartagena <jcartagena93@gmail.com>
2015-10-07 19:28:37 -0400

    adding mynd_reader game

---
+++ mynd_reader.rb
@@ -0,0 +1,198 @@
+def say(statement)
+	puts statement
+end
+
+def ask_question(question)
+	say question
+	gets.chomp.downcase.chr
+end
+
+
+# def handle_operator(chosen_operator)
+# 	case chosen_operator
+# when "add"
+# 	addition_num = rand(1..25)
+# 	ask_question("add #{addition_num} to your current number.. once you've calculated, press ENTER")
+# 	# puts "add #{addition_num} to your current number.. when you've calculated, press ENTER"
+# 	# input = gets.chomp
+# 	current_num += addition_num
+# when "subtract"
+# 	sub_num = rand(1..15)
+# 	ask_question("subtract #{sub_num} from your current number.. once you've calculated, press ENTER")
+# 	# puts "subtract #{sub_num} from your current number.. when you've calculated, press ENTER"
+# 	# input = gets.chomp
+# 	current_num -= sub_num
+#     # when "multiply"
+#     # 	multiply_num = rand(2..4)
+#   	 #  puts "mutiply your current number by #{multiply_num}"
+#   	 #    input = gets.chomp
+#   	 #    current_num *= multiply_num
+#   	 #    puts current_num
+#     # when "division"
+#     # 	div_num = rand(1..4)
+#   	 #  puts "divide your current number by #{div_num}"
+#   	 #    input = gets.chomp
+#   	 #    current_num /= div_num.to_f
+#   	 #    puts current_num
+#   	end
+#   	num_of_directions -= 1 
+# end
+
+
+
+
+
+game_on = true
+while game_on
+
+	say "Muahahaha... welcome to Mynd Reader"
+
+	puts "=" * 50
+
+	puts "          _                  _         
+	_____ _ _ ___ _| |   ___ ___ ___ _| |___ ___ 
+	|     | | |   | . |  |  _| -_| .'| . | -_|  _|
+	|_|_|_|_  |_|_|___|  |_| |___|__,|___|___|_|  
+	   |___|                                   
+	"
+
+	puts "=" * 50
+
+	puts "just a warning.. your mind may be blown.."
+	puts "..."
+	sleep(1)
+	puts "Alright so, let's begin!"
+	puts "Think of a number from (1-10)... once you have that number, MEMORIZE(not type) it and press ENTER."
+	input = gets.chomp
+# until input.include?("yes") # trying to add a way to exit or loop program
+puts "..."
+sleep(2)
+
+
+# puts chosen_operator ## just to check if chosen operator matches output
+
+puts "(small chuckle).. let the games begin.. "
+
+puts "..."
+sleep(2)
+
+
+num_of_directions = rand(4..10)
+
+current_num = 0
+
+
+while num_of_directions >= 0 # picks how many times we perform math
+	operators = ["add", "subtract"]
+#geo_operators = ["multiply", "division"]
+
+if current_num > 5 
+	chosen_operator = operators.sample
+else
+	chosen_operator =  "add"
+end
+
+#handle_operator(chosen_operator)
+case chosen_operator
+when "add"
+	addition_num = rand(1..25)
+	ask_question("add #{addition_num} to your current number.. once you've calculated, press ENTER")
+	# puts "add #{addition_num} to your current number.. when you've calculated, press ENTER"
+	# input = gets.chomp
+	current_num += addition_num
+when "subtract"
+	sub_num = rand(1..15)
+	ask_question("subtract #{sub_num} from your current number.. once you've calculated, press ENTER")
+	# puts "subtract #{sub_num} from your current number.. when you've calculated, press ENTER"
+	# input = gets.chomp
+	current_num -= sub_num
+    # when "multiply"
+    # 	multiply_num = rand(2..4)
+  	 #  puts "mutiply your current number by #{multiply_num}"
+  	 #    input = gets.chomp
+  	 #    current_num *= multiply_num
+  	 #    puts current_num
+    # when "division"
+    # 	div_num = rand(1..4)
+  	 #  puts "divide your current number by #{div_num}"
+  	 #    input = gets.chomp
+  	 #    current_num /= div_num.to_f
+  	 #    puts current_num
+  	end
+  	num_of_directions -= 1 
+
+  end
+
+  if num_of_directions <= 0
+  	ask_question("Alright.. now let's do this... take the number you have in your head and subtract it by the number you picked in the beginning.. when you're ready for some magic.. press ENTER")
+  	# puts "Alright.. now let's do this... take the number you have in your head and subtract it 
+  	# by the number you picked in the beginning.. when you're ready for some magic.. press ENTER"
+  	# input = gets.chomp
+  	puts "Good good.. ok let me think... "
+
+  	puts "..."
+  	sleep(rand(1..3))
+
+
+  	flag = true
+  	flag_2 = true
+  	flag_3 = true
+  	while flag 
+  		# ask_question("is the number you currently have in your head #{current_num}? (yes/no)")    ## didn't work
+  		puts "is the number you currently have in your head #{current_num}? (yes/no)"
+  		input = gets.downcase.chomp.chr
+
+
+  		if input == "y"
+  			flag = false
+  			puts "(;"
+  				# ask_question("would you like to play again? (yes/no)")  ## didn't work
+  				puts "would you like to play again? (yes/no)"
+  				user_answer = gets.downcase.chomp.chr
+
+  				while flag_2
+  					if user_answer  == "y"
+  						 flag_2 = false
+  						 game_on = true								#make method
+  						 num_of_directions = 0
+  					elsif user_answer == "n"
+  						flag_2 = false
+  						puts "Oh ok.. This was fun.. sorry if I scared you."
+  						game_on = false
+  					else
+  						#ask_question("You know, I'm fan of direct answers.. please answer with either a yes or a no (:")    ## didn't work
+  						puts "You know, I'm fan of direct answers.. please answer with either a yes or a no (:"
+  							user_answer = gets.chomp.downcase.chr
+  						end
+  					end
+
+  				elsif input == "n"
+  					flag = false
+            # ask_question("ok so either your math isn't up to par.. or I'm wrong..         ## didn't work
+  					# but I'm a mind reading computer so... how about we try again? (yes/no)")
+  					puts "ok so either your math isn't up to par.. or I'm wrong.. but I'm a mind reading computer so... how about we try again? (yes/no)"
+  					user_answer = gets.downcase.chomp.chr
+
+  					while flag_3
+  						if user_answer == "y"
+  							 flag_3 = false							#make method
+  							 game_on = true
+  							 num_of_directions = 0
+  						elsif user_answer == "n"
+  							flag_3 = false
+  							puts "hey don't worry.. there are plenty of careers where you don't need math."
+  							game_on = false
+  						else
+  							puts "You know, I'm fan of direct answers.. please answer with either a yes or a no (:"
+  								user_answer = gets.chomp.downcase.chr
+  							end
+  						end
+
+  					else
+  						puts "You know, I'm fan of direct answers.. please answer with either a yes or a no (:"
+
+  					end
+  				end
+  			end
+  		end
+
